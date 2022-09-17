cb767a80bfe3cc4e698edaed700c377d5a7585e0
Michael O'Grady <mogrady.professional@gmail.com>
2022-09-17 19:52:38 +0100

    fading header

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { IonContent, IonList, IonSlides, isPlatform } from '@ionic/angular';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage implements OnInit, AfterViewInit {
+  data = null;
 
-  constructor() { }
+  opts = {
+    freeMode: true,
+    slidesPerView: 2.6,
+    slidesOffsetBefore: 30,
+    slidesOffsetAfter: 100,
+  };
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.http
+      .get('https://devdactic.fra1.digitaloceanspaces.com/foodui/1.json')
+      .subscribe((res: any) => {
+        this.data = res;
+      });
   }
 
+  ngAfterViewInit() {}
 }
